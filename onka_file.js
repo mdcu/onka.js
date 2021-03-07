d6ca72720c0bcda1c7d78b506146882416608778
mdcu.firebase@chula.md <mdcu.firebase@chula.md>
2021-03-07 15:25:14 +0700

    new file reader

--- onka_file.js
+++ onka_file.js
@@ -1,4 +1,4 @@
-readFile = function(filePath) {
+readURL = function(filePath) {
   var result = null;
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.open("GET", filePath, false);
@@ -6,6 +6,18 @@ readFile = function(filePath) {
   if (xmlhttp.status==200) {
     return xmlhttp.responseText;
   }
+}
+
+readINPUT = function(e){ // readINPUT(event) in input file
+    console.log(e)
+    var f = e.target.files[0]
+    console.log(f)
+    var reader = new FileReader()
+    reader.onload = function(){
+        let data = reader.result
+        console.log(data)
+    }
+    return reader.readAsText(f)
 }
 
 CSVToArray = function(csv,line_break="\n",delim=","){
